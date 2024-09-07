4fa9fd4c20dcadb0424c29128c97e1639c20ee7a
w-modz <46196436+w-modz@users.noreply.github.com>
2024-09-07 17:55:51 +0200

    Add Spring Boot for dependency injection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 dependencies {
     implementation("org.javacord:javacord:3.8.0")
-    implementation("com.github.masecla22:Reddit4J:-SNAPSHOT")
     runtimeOnly("org.apache.logging.log4j:log4j-core:2.17.0")
+    implementation(libs.spring.boot)
     implementation(project(":connectors:reddit"))
     implementation(project(":domain"))
 }
 
 application {
-    mainClass = "org.example.app.AppKt"
+    mainClass = "com.github.wmodz.discordwebbot.AppKt"
 }
--- connectors/reddit/build.gradle.kts
+++ connectors/reddit/build.gradle.kts
@@ -4,5 +4,6 @@ plugins {
 
 dependencies {
     implementation("com.github.masecla22:Reddit4J:-SNAPSHOT")
+    implementation(libs.spring.boot)
     implementation(project(":domain"))
 }
